4c72b1112ca5a57c8adf1fde1a368ba709d98f30
G6520 <g6520@evertrust.com.tw>
2021-12-13 17:01:28 +0800

    add Distance component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,19 @@
 <template>
-  <div class="home">
-    <Quantity
-      title="可租借"
-      icon="bicycle"
-      :quantity="rent"
-    />
-    <Quantity
-      title="可停車"
-      icon="parking"
-      :quantity="parkingSpace"
-    />
-    <State
-      v-if="rent > 0 || parkingSpace > 0 || !isAvailable"
-      :rent="rent"
-      :parkingSpace="parkingSpace"
-      :isAvailable="isAvailable"
-    />
+  <div class="home container">
+    <Distance :distance="distance" />
+    <Distance :distance="1234" />
   </div>
 </template>
 
 <script setup>
-import Quantity from '@/components/Quantity.vue';
-import State from '@/components/State.vue';
+import Distance from '@/components/Distance.vue';
 
-const rent = 2;
-const parkingSpace = 50;
-const isAvailable = false;
+const distance = 1032;
 </script>
+
+<style lang="scss" scoped>
+.home {
+  text-align: center;
+  border: 1px solid black;
+}
+</style>
