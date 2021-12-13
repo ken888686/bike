2351b94bc749b6a9072ade1ca92b0ef49a8dab59
G6520 <g6520@evertrust.com.tw>
2021-12-13 14:23:02 +0800

    add bootstrap, fortawesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import '@/assets/scss/all.scss';
+import '@fortawesome/fontawesome-free/js/all.min';
+import 'bootstrap';
+
 createApp(App).use(router).mount('#app');
