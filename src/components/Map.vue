cce417d48c09d806a227b9eed92a98f5fa01127b
G6520 <g6520@evertrust.com.tw>
2021-12-15 15:59:32 +0800

    add Map component

---
+++ src/components/Map.vue
@@ -0,0 +1,23 @@
+<template>
+  <l-map
+    class="h-100"
+    :zoom="zoom"
+    :center="center"
+  >
+    <l-tile-layer
+      :url="url"
+      :attribution="attribution"
+    />
+  </l-map>
+</template>
+
+<script setup>
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
+
+const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attribution = '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+const zoom = 8;
+const center = [23.97565, 120.9738819];
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,9 @@
 <template>
-  <l-map
-    class="h-100"
-    :zoom="zoom"
-    :center="center"
-  >
-    <l-tile-layer
-      :url="url"
-      :attribution="attribution"
-    />
-  </l-map>
+  <Map />
 </template>
 
 <script setup>
-import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
-
-const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const attribution = '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors';
-const zoom = 7;
-const center = [23.97565, 120.9738819];
+import Map from '@/components/Map.vue';
 </script>
 
 <style lang="scss" scoped></style>
