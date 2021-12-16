d8507255e059425ccbf213ab6f4539749f80f781
G6520 <g6520@evertrust.com.tw>
2021-12-16 11:45:09 +0800

    adjust SearchList position

--- src/components/SearchList.vue
+++ src/components/SearchList.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="search-list h-100">
-    <SearchGroup />
-    <StationInfo />
-    <StationInfo />
-    <StationInfo />
+  <div class="search-list overflow-auto bg-light">
+    <div class="sticky-top search-list-input">
+      <SearchGroup />
+    </div>
+    <div class="search-list-result">
+      <StationInfo />
+      <StationInfo />
+      <StationInfo />
+      <StationInfo />
+      <StationInfo />
+    </div>
   </div>
 </template>
 <script setup>
 import SearchGroup from './SearchGroup.vue';
 import StationInfo from './StationInfo.vue';
 </script>
-<style lang="scss" scoped></style>
